f6b9c661bd5fb0305c3f5863a5673c2ee6168157
Marc <marcflament@free.fr>
2024-10-20 22:07:30 +0200

    More cudaMemcpy benchmarks

--- src/main/java/org/yah/tools/memcpy/CudaMemcpyBenchmark.java
+++ src/main/java/org/yah/tools/memcpy/CudaMemcpyBenchmark.java
@@ -5,6 +5,7 @@ import com.sun.jna.Pointer;
 import com.sun.jna.ptr.PointerByReference;
 import org.yah.tools.cuda.runtime.RuntimeAPI;
 import org.yah.tools.cuda.runtime.cudaMemcpyKind;
+import org.yah.tools.memcpy.MemcpyKernelsSupport.MemcpyFunctionId;
 
 public class CudaMemcpyBenchmark {
 
@@ -13,7 +14,7 @@ public class CudaMemcpyBenchmark {
     private static final long SIZE = 2 * GB;
     private static final int LENGTH = (int) (SIZE / Long.BYTES);
     private static final int WARMUPS = 3;
-    private static final int RUNS = 10;
+    private static final int RUNS = 5;
     private static final boolean TEST = false;
 
     private static int deviceOrdinal = 0;
@@ -33,21 +34,24 @@ public class CudaMemcpyBenchmark {
 
     private static void test(MemcpyKernelsSupport kernelsSupport, Pointer hostMemory, Pointer deviceMemory) {
         long[] javaMemory = new long[LENGTH];
-        test(kernelsSupport, hostMemory, deviceMemory, javaMemory, false);
-        test(kernelsSupport, hostMemory, deviceMemory, javaMemory, true);
+        test(kernelsSupport, hostMemory, deviceMemory, javaMemory, false, null);
+        test(kernelsSupport, hostMemory, deviceMemory, javaMemory, true, null);
+        for (MemcpyFunctionId functionId : MemcpyFunctionId.values()) {
+            test(kernelsSupport, hostMemory, deviceMemory, javaMemory, true, functionId);
+        }
     }
 
-    private static void test(MemcpyKernelsSupport kernelsSupport, Pointer hostMemory, Pointer deviceMemory, long[] javaMemory, boolean zeroCopy) {
+    private static void test(MemcpyKernelsSupport kernelsSupport, Pointer hostMemory, Pointer deviceMemory, long[] javaMemory,
+                             boolean pinHostMemory, MemcpyFunctionId functionId) {
         if (!TEST)
             return;
-
+        System.out.println("Testing " + label(pinHostMemory, functionId));
         for (int i = 0; i < javaMemory.length; i++) javaMemory[i] = i;
         hostMemory.write(0, javaMemory, 0, LENGTH);
 
-        Pointer hostPtr = zeroCopy ? kernelsSupport.hostRegister(hostMemory, SIZE) : hostMemory;
-        run(kernelsSupport, hostPtr, deviceMemory, zeroCopy);
-        if (zeroCopy)
-            kernelsSupport.hostUnregister(hostMemory);
+        Pointer hostPtr = pinHostMemory ? kernelsSupport.hostRegister(hostMemory, SIZE) : hostMemory;
+        run(kernelsSupport, hostPtr, deviceMemory, functionId);
+        if (pinHostMemory) kernelsSupport.hostUnregister(hostMemory);
 
         hostMemory.read(0, javaMemory, 0, LENGTH);
         int errors = 0;
@@ -55,30 +59,36 @@ public class CudaMemcpyBenchmark {
             if (javaMemory[i] != i * 2L) errors++;
         }
         if (errors > 0)
-            throw new IllegalStateException(label(zeroCopy) + ": " + errors + " errors");
+            throw new IllegalStateException(label(pinHostMemory, functionId) + ": " + errors + " errors");
     }
 
     private static void bench(MemcpyKernelsSupport kernelsSupport, Pointer hostMemory, Pointer deviceMemory) {
         if (RUNS == 0)
             return;
-        bench(kernelsSupport, hostMemory, deviceMemory, false);
-        bench(kernelsSupport, hostMemory, deviceMemory, true);
+        bench(kernelsSupport, hostMemory, deviceMemory, false, null);
+        bench(kernelsSupport, hostMemory, deviceMemory, true, null);
+        for (MemcpyFunctionId functionId : MemcpyFunctionId.values())
+            bench(kernelsSupport, hostMemory, deviceMemory, true, functionId);
     }
 
-    private static void bench(MemcpyKernelsSupport kernelsSupport, Pointer hostMemory, Pointer deviceMemory, boolean zeroCopy) {
-        Pointer hostPtr = zeroCopy ? kernelsSupport.hostRegister(hostMemory, SIZE) : hostMemory;
+    private static void bench(MemcpyKernelsSupport kernelsSupport, Pointer hostMemory, Pointer deviceMemory, boolean pinHostMemory, MemcpyFunctionId functionId) {
+        Pointer hostPtr = pinHostMemory ? kernelsSupport.hostRegister(hostMemory, SIZE) : hostMemory;
 
-        for (int i = 0; i < WARMUPS; i++) run(kernelsSupport, hostPtr, deviceMemory, zeroCopy);
+        for (int i = 0; i < WARMUPS; i++) run(kernelsSupport, hostPtr, deviceMemory, functionId);
 
         MemoryTimes memoryTimes = new MemoryTimes();
-        for (int i = 0; i < RUNS; i++)  memoryTimes.add(run(kernelsSupport, hostPtr, deviceMemory, zeroCopy));
+        long[] javaMemory = new long[LENGTH];
+        for (int i = 0; i < RUNS; i++) {
+            for (int j = 0; j < javaMemory.length; j++) javaMemory[j] = j;
+            hostMemory.write(0, javaMemory, 0, LENGTH);
+            memoryTimes.add(run(kernelsSupport, hostPtr, deviceMemory, functionId));
+        }
 
         long totalSize = SIZE / GB * RUNS;
-        System.out.printf("%s host2device=%.3f GB/s device2host=%.3f GB/s%n", label(zeroCopy),
+        System.out.printf("%-25s host2device=%.3f GB/s device2host=%.3f GB/s%n", label(pinHostMemory, functionId),
                 totalSize / memoryTimes.host2device, totalSize / memoryTimes.device2host);
 
-        if (zeroCopy)
-            kernelsSupport.hostUnregister(hostMemory);
+        if (pinHostMemory) kernelsSupport.hostUnregister(hostMemory);
     }
 
     private static final class MemoryTimes {
@@ -91,12 +101,12 @@ public class CudaMemcpyBenchmark {
         }
     }
 
-    private static MemoryTimes run(MemcpyKernelsSupport kernelsSupport, Pointer hostPtr, Pointer deviceMemory, boolean zeroCopy) {
+    private static MemoryTimes run(MemcpyKernelsSupport kernelsSupport, Pointer hostPtr, Pointer deviceMemory, MemcpyFunctionId functionId) {
         MemoryTimes memoryTimes = new MemoryTimes();
 
         long start = System.nanoTime();
-        if (zeroCopy)
-            kernelsSupport.memcpy(deviceMemory, hostPtr, LENGTH);
+        if (functionId != null)
+            kernelsSupport.memcpy(functionId, deviceMemory, hostPtr, LENGTH);
         else
             cuda.cudaMemcpy(deviceMemory, hostPtr, SIZE, cudaMemcpyKind.cudaMemcpyHostToDevice).check();
         memoryTimes.host2device += (System.nanoTime() - start) * 1E-9;
@@ -104,8 +114,8 @@ public class CudaMemcpyBenchmark {
         kernelsSupport.scale(deviceMemory, LENGTH, 2);
 
         start = System.nanoTime();
-        if (zeroCopy)
-            kernelsSupport.memcpy(hostPtr, deviceMemory, LENGTH);
+        if (functionId != null)
+            kernelsSupport.memcpy(functionId, hostPtr, deviceMemory, LENGTH);
         else
             cuda.cudaMemcpy(hostPtr, deviceMemory, SIZE, cudaMemcpyKind.cudaMemcpyDeviceToHost).check();
         memoryTimes.device2host += (System.nanoTime() - start) * 1E-9;
@@ -119,8 +129,12 @@ public class CudaMemcpyBenchmark {
         return ptrRef.getValue();
     }
 
-    private static String label(boolean zeroCopy) {
-        return zeroCopy ? "zeroCopy" : "cudaMemcpy";
+    private static String label(boolean pinHostMemory, MemcpyFunctionId functionId) {
+        if (functionId != null)
+            return "memcpy_" + functionId;
+        String s = "cudaMemcpy";
+        if (pinHostMemory) s += " (pin host)";
+        return s;
     }
 
     public static void main(String[] args) {
--- src/main/java/org/yah/tools/memcpy/MemcpyKernelsSupport.java
+++ src/main/java/org/yah/tools/memcpy/MemcpyKernelsSupport.java
@@ -20,22 +20,53 @@ public class MemcpyKernelsSupport implements AutoCloseable {
 
     // @formatter:off
     public static final String KERNELS = """
-typedef unsigned int uint32;
-typedef long long int64;
-extern "C" __global__ void scale(int64* data, uint32 length, int factor)
+typedef unsigned int uint;
+
+extern "C" __global__ void scale(long long *data, uint length, int factor)
 {
-    for (uint32 x = blockDim.x * blockIdx.x + threadIdx.x; x < length; x += gridDim.x * blockDim.x)
+    for (uint x = blockDim.x * blockIdx.x + threadIdx.x; x < length; x += gridDim.x * blockDim.x)
     {
         data[x] *= factor;
     }
 }
 
-extern "C" __global__ void memcpy(int64* dst, int64* src, uint32 length)
+template<typename T>
+__device__ void memcpy(T *dst, T *src, uint length)
 {
-    for (uint32 x = blockDim.x * blockIdx.x + threadIdx.x; x < length; x += gridDim.x * blockDim.x)
+    for (uint x = blockDim.x * blockIdx.x + threadIdx.x; x < length; x += gridDim.x * blockDim.x)
     {
         dst[x] = src[x];
     }
+}
+
+extern "C" __global__ void memcpy_int1(int1 *dst, int1 *src, uint length)
+{
+    memcpy<int1>(dst, src, length);
+}
+
+extern "C" __global__ void memcpy_int2(int2 *dst, int2 *src, uint length)
+{
+    memcpy<int2>(dst, src, length);
+}
+
+extern "C" __global__ void memcpy_int4(int4 *dst, int4 *src, uint length)
+{
+    memcpy<int4>(dst, src, length);
+}
+
+extern "C" __global__ void memcpy_longlong1(longlong1 *dst, longlong1 *src, uint length)
+{
+    memcpy<longlong1>(dst, src, length);
+}
+
+extern "C" __global__ void memcpy_longlong2(longlong2 *dst, longlong2 *src, uint length)
+{
+    memcpy<longlong2>(dst, src, length);
+}
+
+extern "C" __global__ void memcpy_longlong4(longlong4 *dst, longlong4 *src, uint length)
+{
+    memcpy<longlong4>(dst, src, length);
 }
             """; // @formatter:on
 
@@ -45,21 +76,69 @@ extern "C" __global__ void memcpy(int64* dst, int64* src, uint32 length)
          */
         CUresult scale(ExecutionConfig executionConfig, Pointer data, int length, int factor);
 
+        CUresult memcpy_int1(ExecutionConfig executionConfig, Pointer dst, Pointer src, int length);
+
+        CUresult memcpy_int2(ExecutionConfig executionConfig, Pointer dst, Pointer src, int length);
+
+        CUresult memcpy_int4(ExecutionConfig executionConfig, Pointer dst, Pointer src, int length);
+
+        CUresult memcpy_longlong1(ExecutionConfig executionConfig, Pointer dst, Pointer src, int length);
+
+        CUresult memcpy_longlong2(ExecutionConfig executionConfig, Pointer dst, Pointer src, int length);
+
+        CUresult memcpy_longlong4(ExecutionConfig executionConfig, Pointer dst, Pointer src, int length);
+    }
+
+    public enum MemcpyFunctionId {
+        int1(0.5),
+        int2(1),
+        int4(2),
+
+        longlong1(1),
+        longlong2(2),
+        longlong4(4);
+
+        public final double nbLongs;
+
+        MemcpyFunctionId(double nbLongs) {
+            this.nbLongs = nbLongs;
+        }
+    }
+
+    @FunctionalInterface
+    interface MemcpyKernelFunction {
         CUresult memcpy(ExecutionConfig executionConfig, Pointer dst, Pointer src, int length);
     }
 
+    public class MemcpyFunction {
+        private final MemcpyFunctionId id;
+        private final MemcpyKernelFunction kernelFunction;
+        private final ExecutionConfig executionConfig;
+
+        private MemcpyFunction(MemcpyFunctionId id, MemcpyKernelFunction kernelFunction, ExecutionConfig executionConfig) {
+            this.id = id;
+            this.executionConfig = executionConfig;
+            this.kernelFunction = kernelFunction;
+        }
+
+        public void run(Pointer dst, Pointer src, int length) {
+            kernelFunction.memcpy(executionConfig, src, dst, (int)(length / id.nbLongs)).check();
+            cuda.cudaDeviceSynchronize().check();
+        }
+    }
+
     private final RuntimeAPI cuda;
-    private final DriverAPI cu;
 
     private final KernelSupport kernelSupport;
     private final Pointer cuModule;
     private final MemcpyKernels kernels;
     private final ExecutionConfig scaleExecutionConfig;
-    private final ExecutionConfig memcpyExecutionConfig;
+
+    private final MemcpyFunction[] memcpyFunctions = new MemcpyFunction[MemcpyFunctionId.values().length];
 
     public MemcpyKernelsSupport(RuntimeAPI cuda, int deviceOrdinal, int blockSize) {
         this.cuda = cuda;
-        this.cu = DriverAPI.load();
+        DriverAPI cu = DriverAPI.load();
 
         kernelSupport = new KernelSupport(deviceOrdinal, cu, NVRTC.load());
         Pointer program = kernelSupport.compile(KERNELS, Map.of());
@@ -79,8 +158,20 @@ extern "C" __global__ void memcpy(int64* dst, int64* src, uint32 length)
         int smCount = smCountRef.getValue();
         int maxBlockParSM = OccupancyHelper.getMaxActiveBlocksPerMultiprocessor(cu, cuModule, "scale", blockSize, 0);
         scaleExecutionConfig = new ExecutionConfig(maxBlockParSM * smCount, 1, 1, blockSize, 1, 1, 0, null);
-        maxBlockParSM = OccupancyHelper.getMaxActiveBlocksPerMultiprocessor(cu, cuModule, "memcpy", blockSize, 0);
-        memcpyExecutionConfig = new ExecutionConfig(maxBlockParSM * smCount, 1, 1, blockSize, 1, 1, 0, null);
+
+        for (MemcpyFunctionId id : MemcpyFunctionId.values()) {
+            maxBlockParSM = OccupancyHelper.getMaxActiveBlocksPerMultiprocessor(cu, cuModule, "memcpy_" + id.name(), blockSize, 0);
+            ExecutionConfig executionConfig = new ExecutionConfig(maxBlockParSM * smCount, 1, 1, blockSize, 1, 1, 0, null);
+            MemcpyKernelFunction kernelFunction = switch (id) {
+                case int1 ->  kernels::memcpy_int1;
+                case int2 ->  kernels::memcpy_int2;
+                case int4 ->  kernels::memcpy_int4;
+                case longlong1 ->  kernels::memcpy_longlong1;
+                case longlong2 ->  kernels::memcpy_longlong2;
+                case longlong4 ->  kernels::memcpy_longlong4;
+            };
+            memcpyFunctions[id.ordinal()] = new MemcpyFunction(id, kernelFunction, executionConfig);
+        }
     }
 
     public Pointer hostRegister(Pointer hostPtr, long size) {
@@ -99,9 +190,9 @@ extern "C" __global__ void memcpy(int64* dst, int64* src, uint32 length)
         cuda.cudaDeviceSynchronize().check();
     }
 
-    public void memcpy(Pointer dst, Pointer src, int length) {
-        kernels.memcpy(memcpyExecutionConfig, dst, src, length).check();
-        cuda.cudaDeviceSynchronize().check();
+    public void memcpy(MemcpyFunctionId id, Pointer dst, Pointer src, int length) {
+        MemcpyFunction memcpyFunction = memcpyFunctions[id.ordinal()];
+        memcpyFunction.run(dst, src , length);
     }
 
     @Override
